2e44b95aea629a65d4a0af42498abe2ff10b3ff5
Michael J. Kazin <mkazin@gmail.com>
2018-03-25 19:29:13 -0400

    Moved duplicate URL-building code to a builder

--- three_games/steamApi.py
+++ three_games/steamApi.py
@@ -26,6 +26,10 @@ class SteamApi(object):
 
     '''
     BASE_URI = 'http://api.steampowered.com/'
+    FUNCTION_FRIEND_LIST = 'ISteamUser/GetFriendList'
+    FUNCTION_OWNED_GAMES = 'IPlayerService/GetOwnedGames'
+    FUNCTION_PLAYER_ACHIEVEMENTS = 'ISteamUserStats/GetPlayerAchievements'
+    FUNCTION_PLAYER_SUMMARIES = 'ISteamUser/GetPlayerSummaries'
 
     def __init__(self, config_file):
         logging.basicConfig(level=logging.DEBUG)
@@ -53,9 +57,7 @@ class SteamApi(object):
 
     def get_player_summaries(self, steamids):
 
-        url = SteamApi.BASE_URI + \
-            'ISteamUser/GetPlayerSummaries/v0002/?key={}&steamids={}&format=json'.format(
-                self.api_key, ','.join((str(x) for x in steamids)))
+        url = SteamApi.build_player_summaries_url(self.api_key, steamids)
 
         logging.debug('get_player_summaries({})'.format(steamids))
         resp = self.__get__(url, PLAYER_SUMARY_LIMIT_EXCEEDED)
@@ -64,15 +66,7 @@ class SteamApi(object):
         return resp.json()['response']['players']
 
     def get_owned_games(self, steamid):
-
-        url = (SteamApi.BASE_URI +
-               'IPlayerService/GetOwnedGames/v0001/' +
-               '?key={}&steamid={}' +
-               '&include_appinfo={}' +
-               '&include_played_free_games={}' +
-               '&format=json').format(
-            self.api_key, steamid,
-            1, 1)
+        url = SteamApi.build_owned_games_url(self.api_key, steam_id, True, True)
 
         logging.debug('get_owned_games({})'.format(steamid))
         resp = self.__get__(url, OWNED_GAMES_LIMIT_EXCEEDED)
@@ -100,13 +94,7 @@ class SteamApi(object):
         relationship : Relationship filter. Possibles values: all, friend
         """
 
-        url = (SteamApi.BASE_URI +
-               'ISteamUser/GetFriendList/v0001/' +
-               '?key={}&steamid={}' +
-               '&relationship={}' +
-               '&format=json').format(
-            self.api_key, steamid,
-            relationship)
+        url = SteamApi.build_friend_list_url(self.api_key, steamid, relationship)
 
         logging.debug('get_friend_list({})'.format(steamid))
         resp = self.__get__(url, FRIEND_LIST_LIMIT_EXCEEDED)
@@ -120,13 +108,8 @@ class SteamApi(object):
 
     def get_player_achievements(self, steamid, app_id):
 
-        url = (SteamApi.BASE_URI +
-               'ISteamUserStats/GetPlayerAchievements/v0001/' +
-               '?key={}&steamid={}' +
-               '&appid={}' +
-               '&l=en' +
-               '&format=json').format(
-            self.api_key, steamid, app_id)
+        url = SteamApi.build_player_achievements_url(
+            self.api_key, steamid, app_id, language='en')
 
         logging.debug('get_player_achievements({}, {})'.format(steamid, app_id))
         resp = self.__get__(url, ACHIEVEMENTS_LIMIT_EXCEEDED)
@@ -156,3 +139,107 @@ class SteamApi(object):
             logging.debug('{} queries left'.format(self.query_limit))
 
         return requests.get(url)
+
+    @staticmethod
+    def build_player_summaries_url(api_key, steam_ids):
+
+        builder = SteamApi.UrlBuilder()
+        builder.with_key(api_key).with_function(SteamApi.FUNCTION_PLAYER_SUMMARIES)
+        builder.with_steam_ids(steam_ids).with_version('0002')
+        return builder.build()
+
+    @staticmethod
+    def build_owned_games_url(
+            api_key, steam_id,
+            include_appinfo=True, include_played_free_games=True):
+
+        builder = SteamApi.UrlBuilder()
+        builder.with_key(api_key)
+        builder.with_steam_id(steam_id)
+        builder.with_function(SteamApi.FUNCTION_OWNED_GAMES)
+        builder.with_version('0001')
+        builder.with_param('include_appinfo', 1 if include_appinfo else 0)
+        builder.with_param('include_played_free_games',
+                           1 if include_played_free_games else 0)
+        return builder.build()
+
+    @staticmethod
+    def build_friend_list_url(
+            api_key, steam_id, relationship):
+
+        builder = SteamApi.UrlBuilder.create(api_key)
+        builder.with_function(SteamApi.FUNCTION_FRIEND_LIST)
+        builder.with_steam_id(steam_id)
+        builder.with_version('0001')
+        builder.with_param('relationship', relationship)
+        return builder.build()
+
+    @staticmethod
+    def build_player_achievements_url(api_key, steam_id, app_id, language='en'):
+
+        builder = SteamApi.UrlBuilder.create(api_key)
+        builder.with_function(SteamApi.FUNCTION_PLAYER_ACHIEVEMENTS)
+        builder.with_steam_id(steam_id)
+        builder.with_version('0001')
+        builder.with_param('appid', app_id)
+        builder.with_param('l', language)
+        return builder.build()
+
+    class UrlBuilder(object):
+        def __init__(self):
+            self.function = None
+            self.version = '0002'
+            self.api_key = None
+            self.steam_id = None
+            self.steam_ids = None
+            self.params = {}
+
+        @staticmethod
+        def create(api_key):
+            instance = SteamApi.UrlBuilder()
+            return instance.with_key(api_key)
+
+        def with_param(self, key, value):
+            self.params[key] = value
+
+        def with_key(self, api_key):
+            self.api_key = api_key
+            return self
+
+        def with_steam_id(self, steam_id):
+            self.steam_id = steam_id
+            return self
+
+        def with_steam_ids(self, steam_ids):
+            self.steam_ids = steam_ids
+            return self
+
+        def with_function(self, function):
+            self.function = function
+            return self
+
+        def with_version(self, version):
+            """ Expects a string, likely of format '0001' or '0002'. """
+            self.version = version
+            return self
+
+        def build(self):
+            """ Build and return the URL. """
+            url = SteamApi.BASE_URI + \
+                '{}/v{}?key={}&format=json'.format(
+                    self.function,
+                    self.version,
+                    self.api_key)
+
+            if self.steam_id:
+                url += '&steamid={}'.format(self.steam_id)
+
+            if self.steam_ids:
+                url += '&steamids={}'.format(
+                    ','.join((str(x) for x in self.steam_ids)))
+
+            # Add free-form params
+            url += ''.join(['&{}={}'.format(key, self.params[key])
+                            for key in self.params])
+
+            return url
